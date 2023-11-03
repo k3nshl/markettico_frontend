d6378e5acfe43c5e085ff8ef20894dd31001c615
charrojas <134308839+charrojas@users.noreply.github.com>
2023-11-03 13:52:28 -0600

    Se comentó porque interfiere con la tabla, pero si quieren se puede descomentar

--- resources/views/administradores/dashboard/index.blade.php
+++ resources/views/administradores/dashboard/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Estilos de Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <link href="assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
