78af640095afb1b92b6ec7981d213c2b952551cc
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-05 19:16:05 -0600

    Update plantilla.blade.php

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Estilos de Bootstrap 5 -->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
-    <link href="assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+    <link href="../assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
    
 
     <!--favicon-->
@@ -309,8 +309,8 @@
     <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
 
     <script src="assets/js/index.js"></script>
-    <script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
-    <script src="assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
+    <script src="../assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="../assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(function() {
             $(".knob").knob();
