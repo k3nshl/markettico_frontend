33836b384e650c2231990165cc684cf5648495b0
YendryVO <149547539+YendryVO@users.noreply.github.com>
2023-11-05 19:41:10 -0600

    Update gestionarPaginas.blade.php

--- resources/views/moderador/gestionarPaginas.blade.php
+++ resources/views/moderador/gestionarPaginas.blade.php
@@ -11,6 +11,7 @@
                 <div class="card-body">
                     <div class="row">
 
+                        
                         <div class="col-md-4">
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <h5 class="text-info"><i class="bx bx-notepad me-2 font-22"></i>Agregar Colección</h5>
