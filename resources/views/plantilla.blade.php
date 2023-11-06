8fe7f55eb8c7766fcd9781b900d9dd1515da4bca
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-05 19:14:16 -0600

    Update plantilla.blade.php

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -294,7 +294,7 @@
 
     <!--end switcher-->
     <!-- Bootstrap JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
     <!--plugins-->
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
