eb653527c30d1d195d1080308291c9714830b5a7
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-05 19:07:02 -0600

    Update plantilla.blade.php

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -12,7 +12,7 @@
    
 
     <!--favicon-->
-    <link rel="icon" href="../assets/images/logo-markettico-icono.png" type="image/png" />
+    <link rel="icon" href="assets/images/logo-markettico-icono.png" type="image/png" />
     <!--plugins-->
     <link href="../assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
     <link href="../assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
@@ -28,10 +28,10 @@
     <link rel="stylesheet" href="../assets/css/dark-theme.css" />
     <link rel="stylesheet" href="../assets/css/semi-dark.css" />
     <link rel="stylesheet" href="../assets/css/header-colors.css" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('../css/style.css') }}">
 
     <!--plugins-->
-    <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+    <link href="../assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
 
 
     <title>MarketTico Admin</title>
@@ -293,23 +293,23 @@
 
     <!--end switcher-->
     <!-- Bootstrap JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
     <!--plugins-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-    <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-    <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-    <script src="assets/plugins/chartjs/js/Chart.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-    <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
-    <script src="assets/plugins/jquery-knob/excanvas.js"></script>
-    <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/plugins/simplebar/js/simplebar.min.js"></script>
+    <script src="../assets/plugins/metismenu/js/metisMenu.min.js"></script>
+    <script src="../assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <script src="../assets/plugins/chartjs/js/Chart.min.js"></script>
+    <script src="../assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="../assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+    <script src="../assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
+    <script src="../assets/plugins/jquery-knob/excanvas.js"></script>
+    <script src="../assets/plugins/jquery-knob/jquery.knob.js"></script>
 
-    <script src="assets/js/index.js"></script>
-    <script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
-    <script src="assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
+    <script src="../assets/js/index.js"></script>
+    <script src="../assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="../assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(function() {
             $(".knob").knob();
