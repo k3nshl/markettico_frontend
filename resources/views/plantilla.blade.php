5fb0acfdd93aa6b367e3cf6742c26d25157e33fc
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-05 19:12:33 -0600

    Update plantilla.blade.php

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -21,11 +21,11 @@
     <link href="assets/css/pace.min.css" rel="stylesheet" />
     <script src="assets/js/pace.min.js"></script>
     <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/app.css" rel="stylesheet">
     <link href="assets/css/icons.css" rel="stylesheet">
     <!-- Theme Style CSS 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('../css/style.css') }}">-->
     <link rel="stylesheet" href="assets/css/dark-theme.css" />
     <link rel="stylesheet" href="assets/css/semi-dark.css" />
     <link rel="stylesheet" href="assets/css/header-colors.css" />
