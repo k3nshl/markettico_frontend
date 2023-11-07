48274abe4c6170deb6f04b5f0a0733884feb58cb
YendryVO <149547539+YendryVO@users.noreply.github.com>
2023-11-07 17:46:42 -0600

    muchas cosas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Administrativo\ControllerDashboard;
 use App\Http\Controllers\Administrativo\ControllerEstados;
+use App\Http\Controllers\Administrativo\ControllerGestionarPagos;
 use App\Http\Controllers\Administrativo\ControllerRoles;
 use App\Http\Controllers\Administrativo\ControllerUsuariosAdministrativos;
 use App\Http\Controllers\Moderador\ControllerGestionarPaginas;
@@ -30,3 +31,5 @@ Route::resource('estados_roles', ControllerRoles::class);
 Route::resource('estados_roles', ControllerEstados::class);
 
 Route::resource('moderador', ControllerGestionarPaginas::class);
+Route::resource('planes', ControllerGestionarPagos::class);
+
