2998d7b4f802ed44050783acd4907333b326e16f
arianahm03 <arihndezmor@gmail.com>
2023-11-07 16:34:42 -0600

    nn

---
+++ resources/views/moderador/gestionarCategorias.blade.php
@@ -0,0 +1,10 @@
+@extends('plantilla')
+
+@section('gestion_nombre', 'Gestión de Categorías ')
+
+@section('contenido')
+
+
+<h5 class="text-dark"><i class="bx bx-notepad me-2 font-22  text-info"></i>HOLA</h5>
+    
+@endsection
