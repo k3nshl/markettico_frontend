1f47858aa164e225d39cc877af286a023a17b109
charrojas <134308839+charrojas@users.noreply.github.com>
2023-11-07 18:49:07 -0600

    Update

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Estilos de Bootstrap 5 -->
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('../assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}">
     
 
