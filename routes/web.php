7e009490b68779b9f5f64e04732ef6fbb45f5018
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-10 18:59:43 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Administrativo\ControllerCategorias;
 use App\Http\Controllers\Administrativo\ControllerHistoriales;
 use App\Http\Controllers\Administrativo\ControllerLogin;
 use App\Http\Controllers\Administrativo\ControllerEstadisticas;
+use App\Http\Controllers\Administrativo\ControllerPlantillasCorreos;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,4 +49,9 @@ Route::resource('login', ControllerLogin::class);
 Route:: get('/verificacion', [ControllerLogin::class,'verificar'])->name('verif');
 
 //Rutas para acceder a las plantillas de los correos
+Route:: get('/plantilla_correo_autenticacion', [ControllerPlantillasCorreos::class,'correo_autenticacion'])->name('correo_autenticacion');
+Route:: get('/plantilla_correo_cambios_politicas', [ControllerPlantillasCorreos::class,'correo_cambios_politicas'])->name('correo_cambios_politicas');
+Route:: get('/plantilla_correo_expiracion_suspension', [ControllerPlantillasCorreos::class,'correo_expiracion_suspension'])->name('correo_expiracion_suspension');
+Route:: get('/plantilla_correo_registro', [ControllerPlantillasCorreos::class,'correo_registro'])->name('correo_registro');
+Route:: get('/plantilla_correo_solicitud', [ControllerPlantillasCorreos::class,'correo_solicitud'])->name('correo_solicitud');
 
