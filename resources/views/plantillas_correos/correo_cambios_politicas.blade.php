210ad6edd3c03b870c1c409e7942f37e017dfc79
Kendall Fernández <kenfernandezr2399@gmail.com>
2023-11-11 19:44:00 -0600

    plantillas cambios politicas y soliciutd

---
+++ resources/views/plantillas_correos/correo_cambios_politicas.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!-- loader-->
+    <link href="assets/css/pace.min.css" rel="stylesheet" />
+    <script src="assets/js/pace.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <link href="assets/css/icons.css" rel="stylesheet">
+    <title>Actualización de Políticas</title>
+</head>
+
+<body class="">
+    <!-- wrapper -->
+    <div class="wrapper">
+        <div class=" d-flex align-items-center justify-content-center">
+            <div class="row">
+                <div class="col-12 col-lg-12 mx-auto">
+                    <div class="card">
+                        <div class="row g-12">
+                            <div class="col-lg-12 border-end">
+                                <div class="card-body">
+                                    <div class="p-5">
+                                        <div class="mb-4 text-center">
+                                            <div>
+                                                <img src="../assets/images/logo-markettico-icono.png" class="logo-icon"
+                                                    alt="logo icon">
+                                            </div>
+                                            <div>
+                                                <h4 class="logo-text">MarketTico</h4>
+                                            </div>
+                                        </div>
+                                        <h2 class="mt-5 font-weight-bold text-center">¡Importante!</h2>
+
+                                        <h4 class="mt-5 font-weight-bold text-center">Actualización de Políticas</h4>
+
+                                        <p class="text-muted">
+                                            <h6 class="text-center">Estimado usuario,</h6>
+                                            Hemos actualizado nuestras políticas para mejorar la seguridad y privacidad
+                                            de nuestros usuarios. Por favor, tómese un momento para revisar los cambios.
+                                        </p>
+
+                                        <!-- Agrega aquí el mensaje representativo sobre la actualización de políticas -->
+
+                                    </div>
+
+                                    <div class="d-grid gap-2 my-5">
+                                        <a type="button" class="btn btn-primary" href="{{route('politicas')}}">Ver
+                                            Políticas</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end wrapper -->
+</body>
+
+</html>
