3eebb0cfc20421e7a6578b2c8ac962820e5e604a
Randall <stuarthc199@gmail.com>
2023-11-15 10:24:50 -0600

    Actualización del metodo index del ControlerRoles
    Envié los datos de todos los roles que se hay insertados en la BD a la vista de roles para que los inserten en la tabla

--- app/Http/Controllers/Administrativo/ControllerRoles.php
+++ app/Http/Controllers/Administrativo/ControllerRoles.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Administrativo;
 
 use App\Http\Controllers\Controller;
+use App\Models\Rol;
 use Illuminate\Http\Request;
 
 class ControllerRoles extends Controller
@@ -12,7 +13,8 @@ class ControllerRoles extends Controller
      */
     public function index()
     {
-        return view('estados_roles.index');
+        $roles = Rol::all();
+        return view('estados_roles.index',compact('roles'));
     }
 
     /**
