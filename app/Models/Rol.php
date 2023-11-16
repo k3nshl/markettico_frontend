417992ef0f203984ffe5b2419a89207ffafb88c6
Randall <stuarthc199@gmail.com>
2023-11-16 09:28:09 -0600

    Creacion del modelo Rol con sus hasMay y belongsTo
    Échele un ojo por que a veces me confundo con esos métodos

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -12,4 +12,19 @@ class Rol extends Model
     protected $table ='roles';
     protected $primaryKey='id_rol';
     public $timestamps=false;
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class, 'estado_id');
+    }
+
+    public function usuariosAdministrativos()
+    {
+        return $this->hasMany(UsuarioAdministrativo::class, 'rol_id');
+    }
+
+    public function historialGestionRoles()
+    {
+        return $this->hasMany(HistorialGestionRoles::class, 'rol_id');
+    }
 }
