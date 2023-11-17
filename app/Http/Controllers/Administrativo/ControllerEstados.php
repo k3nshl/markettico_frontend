ee78c8ad45472a58e4a3c18eaf35fb3bfd2b0d89
jorjanyCh <chjorjanys@gmail.com>
2023-11-16 23:43:19 -0600

    Cambio

--- app/Http/Controllers/Administrativo/ControllerEstados.php
+++ app/Http/Controllers/Administrativo/ControllerEstados.php
@@ -34,10 +34,10 @@ class ControllerEstados extends Controller
         //'nombre' => 'Inactivo',
         //]);
 
-          $item = new Estado();
+     $item = new Estado();
         $item->nombre = 'Activo';
         $item->save();
-        //return $item;
+        return $item;
         return redirect('/estados');
     }
 
